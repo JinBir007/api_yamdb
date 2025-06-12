cfb3bf80306d170ebdaad740cf21ff7609236aeb
Jinbir <jinbir@example.com>
2025-06-12 10:19:50 +0300

    Перенести reviews в api

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import ReviewSerializer, CommentSerializer
+from .models import Review, Comment
+
+
+class ReviewViewSet(ModelViewSet):
+    """ViewSet для модели Review"""""
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    """ViewSet для модели Comment"""
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
